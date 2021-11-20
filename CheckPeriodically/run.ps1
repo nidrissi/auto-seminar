a1edb75f9f557ff5bae96e5a1cb2284be3e5defe
Najib Idrissi-Kaïtouni <najib.idrissi.kaitouni@outlook.com>
2021-11-20 21:48:11 +0100

    it's working!

--- CheckPeriodically/run.ps1
+++ CheckPeriodically/run.ps1
@@ -6,6 +6,7 @@ if (!$env:KOS_Data) {
 }
 
 $Response = Invoke-WebRequest -Uri $env:KOS_Data
+
 if (!$Response.BaseResponse.IsSuccessStatusCode) {
     Write-Error "Fetching KOS_Data failed."
     Write-Error $Response.RawContent
@@ -13,4 +14,18 @@ if (!$Response.BaseResponse.IsSuccessStatusCode) {
 }
 
 $Data = $Response.Content | ConvertFrom-Csv
-Push-OutputBinding -Name Event -Value $Data
+
+foreach ($entry in $Data) {
+    if (!($entry.date -match '(\d\d?)/(\d\d?)/(\d\d\d\d)')) {
+        Write-Error "Malformed entry:"
+        Write-Error $entry
+        exit 3
+    }
+    $day = $matches[1]
+    $month = $matches[2]
+    $year = $matches[3]
+    $entry.date = '{0:d2}-{1:d2}-{2:d4}' -f $day, $month, $year
+}
+
+# This will push several items into the queue. Uniquely identified by their date (hopefully!).
+Push-OutputBinding -Name Incoming -Value $Data
--- ProcessEvent/run.ps1
+++ ProcessEvent/run.ps1
@@ -1,4 +1,113 @@
-param([string] $QueueItem, $TriggerMetadata)
+param($QueueItem, $BlobInput, $TriggerMetadata)
 
-Write-Host "PowerShell queue trigger function processed work item: $QueueItem"
-Write-Host "Queue item insertion time: $($TriggerMetadata.InsertionTime)"
+$ErrorActionPreference = 'Stop'
+
+
+if ($BlobInput) {
+    $differenceCheck = ('date', 'speaker', 'webpage', 'affiliation', 'title', 'abstract-file') `
+    | ForEach-Object {
+        $QueueItem[$_] -Eq $BlobInput[$_]
+    }
+    if ($differenceCheck -NotContains $false) {
+        # The stored state is equal to the new state, so we return.
+        Write-Host "The following item is already dealt with:"
+        $QueueItem | Format-List
+        exit 0
+    }
+}
+
+$BaseUrl = "https://www.imj-prg.fr"
+
+Write-Host "Trying to log-in with the provided credentials."
+
+$LoginData = @{
+    _username = $env:IMJ_login;
+    _password = $env:IMJ_password;
+}
+$LoginResponse = Invoke-WebRequest `
+    -Uri ($BaseUrl + "/gestion/login_check") `
+    -Method Post `
+    -Form $LoginData `
+    -SessionVariable Session
+
+if (!$LoginResponse.BaseResponse.IsSuccessStatusCode) {
+    Write-Error "Something went wrong while logging-in:"
+    Write-Error $LoginResponse
+    exit 1
+}
+
+$TokenResponse = Invoke-WebRequest `
+    -Uri ($BaseUrl + "/gestion/evenement/admin/affEvenement/43") `
+    -WebSession $Session
+
+if (!$TokenResponse.BaseResponse.IsSuccessStatusCode) {
+    Write-Error "Something went wrong while getting the token:"
+    Write-Error $LoginResponse
+    exit 2
+}
+
+$Token = $TokenResponse.InputFields.Where({ $_.id -eq "form__token" }, 'First').value
+Write-Debug "Token: $Token"
+
+if ($BlobInput -and $BlobInput.id) {
+    $QueueItem["id"] = $BlobInput.id
+}
+else {
+    Write-Host "Creating the event."
+
+    $FormData = @{
+        "form[titreSeance]"      = "[K-OS] " + $QueueItem["title"];
+        "form[dateSeance]"       = $QueueItem["date"];
+        "form[heureDeb][hour]"   = "14";
+        "form[heureDeb][minute]" = "0";
+        "form[heureFin][hour]"   = "15";
+        "form[heureFin][minute]" = "0";
+        "form[evenement]"        = "43" # séminaire de topologie = 43
+        "form[_token]"           = $Token;
+        "form[save]"             = "";
+    }
+
+    $CreationFormResponse = Invoke-WebRequest `
+        -Uri ($BaseUrl + "/gestion/evenement/admin/affEvenement/43") `
+        -Method Post `
+        -Form $FormData `
+        -WebSession $Session
+
+    $EventId = $CreationFormResponse.BaseResponse.RequestMessage.RequestUri.Segments | Select-Object -Last 1
+    Write-Debug "Event id: $EventId"
+    $QueueItem["id"] = $EventId
+}
+
+Write-Host "Updating event."
+
+Write-Host "Trying to fetch the abstract..."
+$Abstract = ""
+try {
+    if ($QueueItem["abstract-file"]) {
+        $AbstractResponse = Invoke-WebRequest -Uri ("https://lrobert.perso.math.cnrs.fr/" + $QueueItem["abstract-file"])
+        if ($AbstractResponse.BaseResponse.IsSuccessStatusCode) {
+            $Abstract = $AbstractResponse.Content
+        }
+    }
+}
+catch {}
+
+$FormData += @{
+    "form[salle]"                  = "1016";
+    "form[adresse]"                = "4"; # Sophie Germain
+    "form[diffusion]"              = "https://lrobert.perso.math.cnrs.fr/join-kos.html";
+
+    "form[orateurs][1][nom]"       = $QueueItem["speaker"];
+    "form[orateurs][1][pagePerso]" = $QueueItem["webpage"];
+    "form[orateurs][1][employeur]" = $QueueItem["affiliation"];
+    "form[resume]"                 = $Abstract;
+}
+
+Invoke-WebRequest `
+    -Uri ($BaseUrl + "/gestion/evenement/admin/modifSeance/43/" + $QueueItem["id"]) `
+    -Method Post `
+    -Form $FormData `
+    -WebSession $Session `
+| Out-Null
+
+Push-OutputBinding -Name BlobOutput -Value $QueueItem
